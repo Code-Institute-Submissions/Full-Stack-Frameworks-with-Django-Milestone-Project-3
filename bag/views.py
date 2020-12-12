d712b485ce9c4b9c0182d500056cc6e80cdf80bb
SalvoBevi <salvo.bevi@gmail.com>
2020-12-12 16:05:49 +0000

    Fixed Remove funciont into Shopping Bag

--- bag/views.py
+++ bag/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -45,7 +47,7 @@ def remove_from_bag(request, item_id):
     try:
         bag = request.session.get('bag', {})
 
-        bag.pop(item_id)  # remove the id
+        bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
