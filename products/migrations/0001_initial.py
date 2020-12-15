3b74a1c50dff01fda737229e080dec19e15f849b
SalvoBevi <salvo.bevi@gmail.com>
2020-12-15 16:05:05 +0000

    Created Check Out App and Models

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -24,6 +24,7 @@ class Migration(migrations.Migration):
             name='Product',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('sku', models.CharField(blank=True, max_length=254, null=True)),
                 ('name', models.CharField(max_length=254)),
                 ('description', models.TextField(max_length=350)),
                 ('price', models.DecimalField(decimal_places=2, max_digits=6)),
