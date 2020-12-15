2df351d4933702d4f4d7d953505ae79e05ebcbed
SalvoBevi <salvo.bevi@gmail.com>
2020-12-15 11:37:09 +0000

    Edited Shopping Bag Preview

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -24,9 +24,8 @@ class Migration(migrations.Migration):
             name='Product',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('sku', models.CharField(blank=True, max_length=254, null=True)),
                 ('name', models.CharField(max_length=254)),
-                ('description', models.TextField()),
+                ('description', models.TextField(max_length=350)),
                 ('price', models.DecimalField(decimal_places=2, max_digits=6)),
                 ('rating', models.DecimalField(blank=True, decimal_places=2, max_digits=6, null=True)),
                 ('image_url', models.URLField(blank=True, max_length=1024, null=True)),
