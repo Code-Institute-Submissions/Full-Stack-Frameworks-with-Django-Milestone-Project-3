54390f181c0f48add976fa2ffba76aaf8b9aacc4
SalvoBevi <salvo.bevi@gmail.com>
2020-12-16 12:55:26 +0000

    Added Checkout Success Logic

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
@@ -65,6 +65,3 @@ class OrderLineItem(models.Model):
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
