eb9b5742f4decfe65cfa1078bab4c731a07c2d54
SalvoBevi <salvo.bevi@gmail.com>
2020-12-16 10:41:16 +0000

    Added Stripe Element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Hyx6eGf0g0TDPBt22uLR5MWgpxmGxEj859S5YfAKWMDLUi6H4GHzT496IMa0MVEzemSj6lCfKZqYshRiQ5HcFYn00aNdPHsCa',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
